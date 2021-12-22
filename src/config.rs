facf861ad6db2db1d60c0c48c305ab134c6a3cb5
zTgx <747674262@qq.com>
2021-12-22 16:19:54 +0800

    add path /user

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,7 @@ use actix_web::web;
 pub fn config_services(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/v2")
-            .service(web::resource("/login").route(web::post().to(login)))
-            .service(web::resource("/logout").route(web::post().to(logout))),
+            .service(web::resource("/user/login").route(web::post().to(login)))
+            .service(web::resource("/user/logout").route(web::post().to(logout))),
     );
 }
